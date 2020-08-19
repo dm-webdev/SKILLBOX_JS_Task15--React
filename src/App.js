73d9e84e4832baf5964ae914e719063cba226cf8
dm-webdev <denis.mironenko@mail.ru>
2020-08-19 23:29:55 +0300

    plus tool-card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './modules/start/logo.svg';
+import './modules/start/App.css';
+import './modules/catalog/catalog.css';
+import catalog from '../src/assets/database';
+
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +23,54 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <section className="catalog">
+        <h1 className="catalog__header">Каталог электроинструмента</h1>
+
+        <ul className="tool-list">
+          <li className="tool-card">
+            <img src="img/jigsaw.jpg" className="tool__img" alt={catalog.[0].toolName}/>
+
+            <div className="tool__label">
+              <div className="tool__desc">
+                <h2 className="tool__name">{catalog[0].toolName}</h2>
+
+                <p className="tool__text">{catalog[0].toolText}</p>
+              </div>
+
+              <div className="tool__tally">
+                <p className="tool__price">{catalog[0].price} руб/шт</p>
+
+                <p className="tool__availability">{(catalog[0].quantity !== 0) ? 'В наличии' : "Нет в наличии"}</p>
+
+                <p className="tool__quantity">{catalog[0].quantity} шт</p>
+              </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+          </li>
+
+        </ul>
+
+
+
+
+
+
+      </section>
+
+
+
+
+
     </div>
   );
 }
